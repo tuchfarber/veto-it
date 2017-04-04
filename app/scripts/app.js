70837c0fb569f4232c161cf6a6c90fd0c816824a
Matt Tuchfarber <matt@tuchfarber.com>
2017-04-04 01:34:03 -0400

    Sharing now works. However data must be manually refreshed using sharedSession.getData(). Needs update system.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,8 +2,7 @@ window.onload = function(){
     locations = new Vue({
         el: '#locations',
         data: {
-            places: [],
-            locallyDeletedLocations: []
+            places: []
         },
         methods:{
             createMarker: function(place){
@@ -13,14 +12,30 @@ window.onload = function(){
                 });
             },
             removeMarker: function(id){
+                var changed = false;
                 locations.places.forEach(function(current_element, current_index){
                     if(current_element.Place.id == id){
                         current_element.Marker.setMap(null);
                         locations.places.splice(current_index,1);
+                        changed = true;
                         return;
                     }
                 });
-                locations.locallyDeletedLocations.push(id);
+                if(changed){
+                    sharedSession.deletedIds = sharedSession.deletedIds.concat([id]).filter(sharedSession.findUnique)
+                    var veto_url = 'https://api.tuchfarber.com/vetoit/veto/' + sharedSession.sessionId + '/' + sharedSession.sessionKey;
+                    var session_data = {"del_ids":sharedSession.deletedIds}
+                    this.$http.post(veto_url, session_data).then(
+                        function(response){
+                            console.log("Successfully vetoed")
+                        },
+                        function(reponse){
+                            console.log("Error");
+                            console.log(response);
+                        }
+                    );
+                }
+
             }
         }
     });
@@ -60,13 +75,13 @@ window.onload = function(){
                 locations.places.forEach(function(current_element){
                     current_element.Marker.setMap(null);
                 })
-                locations.places = [];
-
+                
                 //Find places
                 this.findPlaces();
             },
             findPlaces: function(){
                 var service = new google.maps.places.PlacesService(map.gmap);
+                locations.places = [];
                 service.nearbySearch({
                     location: this.location,
                     radius: selectedDistance.selected,
@@ -74,10 +89,12 @@ window.onload = function(){
                 }, function(results, status, pagination){
                     if (status === google.maps.places.PlacesServiceStatus.OK) {
                         results.forEach(function(current_element){
-                            locations.places.push({
-                                "Marker": locations.createMarker(current_element),
-                                "Place": current_element
-                            })
+                            if(sharedSession.deletedIds.indexOf(current_element.id) < 0){
+                                locations.places.push({
+                                    "Marker": locations.createMarker(current_element),
+                                    "Place": current_element
+                                })
+                            }
                         })
                     }
                     if(pagination.hasNextPage){
@@ -122,12 +139,12 @@ window.onload = function(){
                 var session_data = {
                     "gps": map.gmap.center.lat() + ',' + map.gmap.center.lng(),
                     "init_km": selectedDistance.selected,
-                    "del_ids": locations.locallyDeletedLocations
+                    "del_ids": sharedSession.deletedIds
                 };
                 this.$http.post('https://api.tuchfarber.com/vetoit/create', session_data).then(
                     function(response){
-                        var share_url = window.location + '#' + response.data.d_id;
-                        window.location.hash = response.data.d_id;
+                        var share_url = window.location + '#' + response.data.d_id + '/' + response.data.d_key;
+                        window.location.hash = response.data.d_id + '/' + response.data.d_key;
                         alert(share_url);
                     },
                     function(reponse){
@@ -142,35 +159,73 @@ window.onload = function(){
     sharedSession = new Vue({
         data: {
             sharedSession: window.location.hash.substring(1) !== "" ? true : false,
-            sessionId: window.location.hash.substring(1),
+            sessionId: window.location.hash.substring(1, window.location.hash.indexOf('/')),
+            sessionKey: window.location.hash.substring(window.location.hash.indexOf('/') + 1),
             deletedIds: [],
             gpsLocation: {lat: 0, lng: 0},
             km: 1000
         },
         methods:{
             getData: function(){
-
+                var retrieve_url = 'https://api.tuchfarber.com/vetoit/retrieve/' + sharedSession.sessionId + '/' + sharedSession.sessionKey;
+                this.$http.get(retrieve_url).then(
+                    function(response){
+                        if(response.status != "Error"){
+                            console.log(response)
+                            selectedDistance.selected = response.data.init_km;
+                            sharedSession.deletedIds = sharedSession.deletedIds.concat(response.data.del_ids).filter(this.findUnique)
+                            var gps = response.data.gps.split(',');
+                            map.changeLocation(parseFloat(gps[0]),parseFloat(gps[1]));
+                        }else{
+                            console.log("Retrieval error: " + response.status_text);
+                            return;
+                        }
+                    },
+                    function(reponse){
+                        console.log("Error");
+                        console.log(response);
+                    }
+                );
+            },
+            findUnique : function(value, index, self) { 
+                return self.indexOf(value) === index;
+            }
+        },
+        watch:{
+            deletedIds: function(ids){
+                ids.forEach(function(id){
+                    locations.removeMarker(id);
+                });
             }
         }
     });
-
-    map.initializeMap();
-    navigator.geolocation.getCurrentPosition(
-        function(position){
-            //Success
-            map.changeLocation(position.coords.latitude,position.coords.longitude);
-        }, function(error){
-            // Error
-            switch(error.code){
-                case 1:
-                    alert("Not permitted to access location data");
-                    break;
-                case 2:
-                    alert("Unable to determine location");
-                    break;
-                case 3:
-                    alert("Location data timeout");
-                    break;
+    window.addEventListener("hashchange", function(hash){
+        sharedSession.sharedSession = window.location.hash.substring(1) !== "" ? true : false;
+        sharedSession.sessionId = window.location.hash.substring(1,window.location.hash.indexOf('/'))
+        sharedSession.sessionKey = window.location.hash.substring(window.location.hash.indexOf('/') + 1)
+    });
+    if(sharedSession.sharedSession){
+        sharedSession.getData()
+    }else{
+        navigator.geolocation.getCurrentPosition(
+            function(position){
+                //Success
+                map.changeLocation(position.coords.latitude,position.coords.longitude);
+            }, function(error){
+                // Error
+                switch(error.code){
+                    case 1:
+                        alert("Not permitted to access location data");
+                        break;
+                    case 2:
+                        alert("Unable to determine location");
+                        break;
+                    case 3:
+                        alert("Location data timeout");
+                        break;
+                }
             }
-        });
+        );
+    }
+    map.initializeMap();
 }
